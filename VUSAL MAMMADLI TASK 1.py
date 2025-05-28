9afc4169a21d547e5d11ce689e35f050f85f4288
Vusal Mammadli <vusalm1402@gmail.com>
2025-05-28 19:19:13 +0400

    Add files via upload
    My first Python code task for QA interview

---
+++ VUSAL MAMMADLI TASK 1.py
@@ -0,0 +1,16 @@
+1.Task 1
+Number = int(input( "Enter a number:"))
+if number > 7:
+print("Hello")
+
+
+2.Task
+
+
+
+input ("Enter a name")
+if name == ("John"):
+    print("Hello John")
+
+    if not
+        print("there is no name")
