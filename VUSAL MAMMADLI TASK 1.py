52a0b04096e662624409c3275b6ecdf0dce6c25d
Vusal Mammadli <vusalm1402@gmail.com>
2025-06-04 20:47:36 +0400

    Update vusal_mammadli_task_updated.py

--- VUSAL MAMMADLI TASK 1.py
+++ VUSAL MAMMADLI TASK 1.py
@@ -1,16 +1,10 @@
 1.Task 1
-Number = int(input( "Enter a number:"))
+Number = int(input("Enter a number:"))
 if number > 7:
 print("Hello")
-
-
 2.Task
-
-
-
 input ("Enter a name")
 if name == ("John"):
     print("Hello John")
-
-    if not
-        print("there is no name")
+if not
+    print("there is no name")
